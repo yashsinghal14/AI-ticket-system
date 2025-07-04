b775138023f6b95ead54369b089b957a8c9b55c5
yash <yashsinghal14@gmail.com>
2025-07-04 13:56:57 +0530

    minor spell change

--- ai-ticket-assistant/controllers/user.js
+++ ai-ticket-assistant/controllers/user.js
@@ -6,7 +6,7 @@ import { inngest } from "../inngest/client.js";
 export const signup = async (req, res) => {
   const { email, password, skills = [] } = req.body;
   try {
-    const hashed = brcypt.hash(password, 10);
+    const hashed = bcrypt.hash(password, 10);
     const user = await User.create({ email, password: hashed, skills });
 
     //Fire inngest event
@@ -36,7 +36,7 @@ export const login = async (req, res) => {
     const user = User.findOne({ email });
     if (!user) return res.status(401).json({ error: "User not found" });
 
-    const isMatch = await brcypt.compare(password, user.password);
+    const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
       return res.status(401).json({ error: "Invalid credentials" });
